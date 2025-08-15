4c261fa70074798ae089365ce2fcd0b5a2a06920
Douglas Aledi <DouglasAledi>
2025-08-14 21:24:13 -0300

    Utilizando um package do postgresql

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=5698 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
 
 type Produto struct {
 	Nome       string
@@ -15,6 +27,8 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html")) // Encapsula todos os templates e retorna dois valores o template e o erro, template.ParseGlob() caminho de onde estão os templates, "templates/*.index.html", *.html, pega todos os arquivos em html
 
 func main() {
+	db := conectaComBancoDeDados()
+	defer db.Close()
 	http.HandleFunc("/", index) //(qual requisição você quer atender, quem atenderá essa requisição)
 
 	http.ListenAndServe(":8000", nil) //ouça e sirva, porta, não passa nem uma informação
